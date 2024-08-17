f8d1a606b0841f9d87881ef7c296ed7e917b4517
Yigithan <xayekk@gmail.com>
2024-08-17 17:45:27 +0300

    complete profile page UI

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,11 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "mern-blog-417b3.firebaseapp.com",
   projectId: "mern-blog-417b3",
   storageBucket: "mern-blog-417b3.appspot.com",
