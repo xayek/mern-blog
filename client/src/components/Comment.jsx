ed17c23df6ffdd93d1664a29acfa9bf56c68ad8d
Yigithan <xayekk@gmail.com>
2024-08-19 17:36:04 +0300

    add delete functionality to the comment section

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -4,7 +4,7 @@ import { FaThumbsUp } from 'react-icons/fa';
 import { useSelector } from 'react-redux';
 import { Button, Textarea } from 'flowbite-react';
 
-export default function Comment({ comment, onLike, onEdit }) {
+export default function Comment({ comment, onLike, onEdit, onDelete }) {
   const [user, setUser] = useState({});
   const [isEditing, setIsEditing] = useState(false);
   const [editedContent, setEditedContent] = useState(comment.content);
@@ -32,14 +32,14 @@ export default function Comment({ comment, onLike, onEdit }) {
 
   const handleSave = async () => {
     try {
-      const res = await fetch(`/api/comment/editComment/${comment._id}`, {
+        const res = await fetch(`/api/comment/editComment/${comment._id}`, {
         method: 'PUT',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          content: editedContent
-        })
+            content: editedContent,
+        }),
       });
       if (res.ok) {
         setIsEditing(false);
@@ -48,6 +48,7 @@ export default function Comment({ comment, onLike, onEdit }) {
     } catch (error) {
       console.log(error.message);
     }
+
   };
 
   return (
@@ -70,12 +71,12 @@ export default function Comment({ comment, onLike, onEdit }) {
         </div>
         {isEditing ? (
           <>
-            <Textarea
+             <Textarea
               className='mb-2'
               value={editedContent}
               onChange={(e) => setEditedContent(e.target.value)}
             />
-            <div className="flex justify-end gap-2 text-xs">
+            <div className='flex justify-end gap-2 text-xs'>
               <Button
                 type='button'
                 size='sm'
@@ -118,13 +119,22 @@ export default function Comment({ comment, onLike, onEdit }) {
               </p>
               {currentUser &&
                 (currentUser._id === comment.userId || currentUser.isAdmin) && (
-                  <button
-                    type='button'
-                    onClick={handleEdit}
-                    className='text-gray-400 hover:text-blue-500'
-                  >
-                    Edit
-                  </button>
+                    <>
+                    <button
+                      type='button'
+                      onClick={handleEdit}
+                      className='text-gray-400 hover:text-blue-500'
+                    >
+                      Edit
+                    </button>
+                    <button
+                      type='button'
+                      onClick={() => onDelete(comment._id)}
+                      className='text-gray-400 hover:text-red-500'
+                    >
+                      Delete
+                    </button>
+                  </>
                 )}
             </div>
           </>
