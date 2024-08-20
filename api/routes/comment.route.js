f5f956b14737813fb8a7476aaf912a976ef04da1
Yigithan <xayekk@gmail.com>
2024-08-20 13:42:32 +0300

    add comments section to the admin dashboard

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -5,7 +5,9 @@ import {
     deleteComment,
     editComment,
     getPostComments,
+    getcomments,
     likeComment,
+    
   } from '../controllers/comment.controller.js';
 
 const router = express.Router();
@@ -16,5 +18,6 @@ router.get('/getPostComments/:postId', getPostComments);
 router.put('/likeComment/:commentId', verifyToken, likeComment);
 router.put('/editComment/:commentId', verifyToken, editComment);
 router.delete('/deleteComment/:commentId', verifyToken, deleteComment);
+router.get('/getcomments', verifyToken, getcomments);
 
 export default router;
--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,5 +1,11 @@
 import { Sidebar } from 'flowbite-react';
-import { HiUser, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup } from 'react-icons/hi';
+import {
+  HiUser,
+  HiArrowSmRight,
+  HiDocumentText,
+  HiOutlineUserGroup,
+  HiAnnotation,
+} from 'react-icons/hi';
 import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { signoutSuccess } from '../redux/user/userSlice';
@@ -60,6 +66,7 @@ export default function DashSidebar() {
             </Link>
           )}
           {currentUser.isAdmin && (
+            <>
             <Link to='/dashboard?tab=users'>
               <Sidebar.Item
                 active={tab === 'users'}
@@ -69,6 +76,16 @@ export default function DashSidebar() {
                 Users
               </Sidebar.Item>
             </Link>
+            <Link to='/dashboard?tab=comments'>
+              <Sidebar.Item
+                active={tab === 'comments'}
+                icon={HiAnnotation}
+                as='div'
+              >
+                Comments
+              </Sidebar.Item>
+            </Link>
+          </>
           )}
           <Sidebar.Item
             icon={HiArrowSmRight}
